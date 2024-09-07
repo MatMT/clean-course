311de4b910742be547bc37949714ba4bb43dad39
EliDev <oscarmateoelias@gmail.com>
2024-09-06 23:18:03 -0600

    Refactorizando Código siguiendo las referencias de Clean Code

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,53 @@
+(() => {
+
+    // Ejemplo
+    const filesToEvaluate = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
+    ];  
+    
+    const filesToDelete = filesToEvaluate.map( file => file.flagged );
+
+    // Malos nombres 
+    class AbstractUser { };
+    class UserMixin { };
+    class UserImplementation { };
+    interface IUser { };
+
+    // Mejor
+    class User { };
+    interface User { };
+
+
+    // Todo: Tarea
+        
+    // día de hoy - today
+    const currentDay = new Date();
+    
+    // días transcurridos - elapsed time in days
+    const elapsedTimeInDays: number = 23;
+    
+    // número de archivos en un directorio - number of files in directory
+    const filesNumberInDirectory = 33;
+    
+    // primer nombre - first name
+    const firstName = 'Fernando';
+    
+    // primer apellido - last name
+    const lastName = 'Herrera';
+
+    // días desde la última modificación - days since modification
+    const daysSinceLastModification = 12;
+    
+    // cantidad máxima de clases por estudiante - max classes per student
+    const maxClassesPerStudent = 6;
+
+
+})();
+
+
+
